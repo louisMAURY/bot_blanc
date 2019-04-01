578a70eed13103f1cc3b64f91b5d5170ca293276
Louis <louismaury33@gmail.com>
2019-04-02 00:08:28 +0200

    Ce sera mieux comme ça !

--- blanc.py
+++ blanc.py
@@ -2,12 +2,9 @@ from discord import *
 
 token = "NTYyMzU0NjAwNDk4NDI5OTc5.XKJlXw.32K4BXIlH2GDd8JS1j6zHDUw0aQ"
 client = Client()
-
-liste_blanc = ["Riz" , "riz" , "Mariage" , "mariage" , "Vin" , "vin" , "Requin" , "requin" , "Laurent" , "laurent" , 
-"Le Paradis" , "le paradis" , "Michel" , "michel" , "Fromage" , "fromage" , "Marine LePen aime que les" , "marine lepen aime que les",
-"Boudin" , "boudin" , "Chocolat" , "chocolat" , "Vote" , "vote" , "Gandalf Le" , "gandalf le" , "Balle à" , "balle à" , "Croc" , "croc" ,
-"Cheval" , "cheval" , "Chevalier" , "chevalier" , "Haricot" , "haricot" , "Céleri" , "celeri" , "céleri" , "Celeri" , "Drapeau" , "drapeau" ,
-"Mont" , "mont" , "Rasé a" , "Rase a" , "rasé a" , "rase a" , "Sable" , "sable" , "Poivre" , "poivre"]
+with open("liste_de_blanc.txt" , "r" , encoding="utf8") as fichier:
+    content = fichier.read();
+    liste_blanc = content.split("\n")
 
 @client.event
 async def on_ready():
