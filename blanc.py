c554bff441b31f725ebd3ca520a9b64fc9aabec2
Louis <louismaury33@gmail.com>
2019-04-03 11:05:18 +0200

    C'est encore mieux !
    Maintenant, quand un message se termine par un mot dans la liste, le bot répond !

--- blanc.py
+++ blanc.py
@@ -1,21 +1,23 @@
-from discord import *
+import discord
 
 token = "NTYyMzU0NjAwNDk4NDI5OTc5.XKJlXw.32K4BXIlH2GDd8JS1j6zHDUw0aQ"
-client = Client()
+client = discord.Client()
 with open("liste_de_blanc.txt" , "r" , encoding="utf8") as fichier:
-    content = fichier.read();
-    liste_blanc = content.split("\n")
+    contenu = fichier.read()
+    liste_blanc = contenu.split("\n")
 
 @client.event
 async def on_ready():
-    print("The bot is runnig!")
-    await client.change_presence(game=Game(name="Jeu du ? BLANC!"))
+    print("The bot is running!")
+    await client.change_presence(game=discord.Game(name="Jeu du ? BLANC!"))
 
 @client.event
 async def on_message(message):
+    message_utilisateur = message.content.lower()
+
     if message.author == client.user:
         return
-    if message.content in liste_blanc:
+    if message_utilisateur.endswith(tuple(liste_blanc)):
         await client.send_message(message.channel, "BLANC !")
         await client.send_file(message.channel, "blanc.png")
 
